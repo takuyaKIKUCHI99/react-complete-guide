3351918ca46a6f5a8f5f715a929ffaa37e49ee4d
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-09-23 18:04:25 +0900

    [FEATURE] Add Person component with props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
+      <Person name="Takuya" age="30" />
+      <Person name="Fumiko" age="29" />
     </div>
   );
 }
