53d1436aef25e2def05fde972a32e452af18b7c2
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-09-24 12:01:07 +0900

    [FEATURE] onClick

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <Person name="Takuya" age="30" />
-      <Person name="Fumiko" age="29">My Hobbies: Racing</Person>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {name: "Takuya", age: "30"},
+      {name: "Fumiko", age: "29"}
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'TK', age: '30' },
+        { name: 'Fumiko', age: '29' }
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hi, I'm a React App</h1>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}>
+          My Hobbies: Racing
+        </Person>
+        <button onClick={this.switchNameHandler} >Switch Name</button>
+      </div>
+    );
+  }
 }
 
 export default App;
